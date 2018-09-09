74932b6e0744c87543e40091d363b8215b9848a2
hossim0 <mn.hossein.x@gmail.com>
2018-09-09 13:48:06 +0430

    meta changes

--- content/meta/config.js
+++ content/meta/config.js
@@ -1,28 +1,28 @@
 module.exports = {
-  siteTitle: "HeroBlog - a GatsbyJS starter", // <title>
-  shortSiteTitle: "HeroBlog - GatsbyJS starter", // <title> ending for posts and pages
-  siteDescription: "HeroBlog is a GatsbyJS starter.",
+  siteTitle: "Hossein Moayed", // <title>
+  shortSiteTitle: "Hossein Moayed", // <title> ending for posts and pages
+  siteDescription: "Personal Website of Hossein Moayed.",
   siteUrl: "https://beta.hossimo.com",
   pathPrefix: "",
   siteImage: "preview.jpg",
   siteLanguage: "en",
   // author
-  authorName: "greg lobinski",
-  authorTwitterAccount: "greglobinski",
+  authorName: "Hossein Moayed",
+  authorTwitterAccount: "hossimo_",
   // info
-  headerTitle: "greg lobinski",
-  headerSubTitle: "presents another one GatsbyJS starter",
+  headerTitle: "Hossein Moayed",
+  headerSubTitle: "Digital Sculptor And Game Character Artist",
   // manifest.json
-  manifestName: "HeroBlog - a GatsbyJS starter",
-  manifestShortName: "HeroBlog", // max 12 characters
+  manifestName: "Hossein Moayed",
+  manifestShortName: "Hossimo", // max 12 characters
   manifestStartUrl: "/index.html",
   manifestBackgroundColor: "white",
   manifestThemeColor: "#666",
   manifestDisplay: "standalone",
   // social
   authorSocialLinks: [
-    { name: "github", url: "https://github.com/greglobinski" },
-    { name: "twitter", url: "https://twitter.com/greglobinski" },
-    { name: "facebook", url: "http://facebook.com/greglobinski" }
+    { name: "github", url: "https://github.com/hossim0" },
+    { name: "twitter", url: "https://twitter.com/hossimo_" },
+    { name: "facebook", url: "http://facebook.com/HosseinTN2" }
   ]
 };
